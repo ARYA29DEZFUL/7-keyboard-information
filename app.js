fac6d6419f350f2f89ed6aa000592e350ef6358b
Aria <ARYAir.121212@gmail.com>
2025-05-15 10:25:24 +0330

    complete projict

--- app.js
+++ app.js
@@ -8,10 +8,40 @@ const Location = $.getElementById("location")
 const which = $.getElementById("which")
 const code = $.getElementById("code")
 
-console.log(code)
+let flag = ""
+
+key.addEventListener("click", () => {
+    flag = "key"
+    mainInformation.textContent = key.textContent
+})
+
+Location.addEventListener("click", () => {
+    flag = "Location"
+    mainInformation.textContent = Location.textContent
+})
+
+which.addEventListener("click", () => {
+    flag = "which"
+    mainInformation.textContent = which.textContent
+})
+
+code.addEventListener("click", () => {
+    flag = "code"
+    mainInformation.textContent = code.textContent
+})
 
 Body.addEventListener("keydown", (event) => {
-    mainInformation.textContent = event.key
+
+    if (!flag) {
+        flag = "key"
+    }
+
+    switch (flag) {
+        case "key": mainInformation.textContent = event.key; break;
+        case "Location": mainInformation.textContent = event.location; break;
+        case "which": mainInformation.textContent = event.which; break;
+        case "code": mainInformation.textContent = event.code; break;
+    }
     key.textContent = event.key
     Location.textContent = event.location
     which.textContent = event.which
